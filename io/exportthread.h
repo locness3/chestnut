2b097b79867afad7d3ec5e14ca2c502f001080f8
Jonathan Noble <jonnobleuk@gmail.com>
2019-01-26 19:12:29 +0000

    Initialisation of members removed from the body of the constructor

--- io/exportthread.h
+++ io/exportthread.h
@@ -1,7 +1,7 @@
-/* 
+/*
  * Olive. Olive is a free non-linear video editor for Windows, macOS, and Linux.
  * Copyright (C) 2018  {{ organization }}
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -34,7 +34,7 @@ struct SwsContext;
 struct SwrContext;
 
 extern "C" {
-	#include <libavcodec/avcodec.h>
+#include <libavcodec/avcodec.h>
 }
 
 #define COMPRESSION_TYPE_CBR 0
@@ -43,67 +43,67 @@ extern "C" {
 #define COMPRESSION_TYPE_TARGETBR 3
 
 class ExportThread : public QThread {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	ExportThread();
-	void run();
+    ExportThread();
+    void run();
 
-	// export parameters
-	QString filename;
-	bool video_enabled;
-	int video_codec;
-	int video_width;
-	int video_height;
-	double video_frame_rate;
-	int video_compression_type;
-	double video_bitrate;
-	bool audio_enabled;
-	int audio_codec;
-	int audio_sampling_rate;
-	int audio_bitrate;
-	long start_frame;
-	long end_frame;
+    // export parameters
+    QString filename;
+    bool video_enabled;
+    int video_codec;
+    int video_width;
+    int video_height;
+    double video_frame_rate;
+    int video_compression_type;
+    double video_bitrate;
+    bool audio_enabled;
+    int audio_codec;
+    int audio_sampling_rate;
+    int audio_bitrate;
+    long start_frame;
+    long end_frame;
 
-	QOffscreenSurface surface;
+    QOffscreenSurface surface;
 
-	ExportDialog* ed;
+    ExportDialog* ed;
 
-	bool continueEncode;
+    bool continueEncode = true;
 signals:
-	void progress_changed(int value, qint64 remaining_ms);
+    void progress_changed(int value, qint64 remaining_ms);
 public slots:
-	void wake();
+    void wake();
 private:
-	bool encode(AVFormatContext* ofmt_ctx, AVCodecContext* codec_ctx, AVFrame* frame, AVPacket* packet, AVStream* stream, bool rescale);
-	bool setupVideo();
-	bool setupAudio();
-	bool setupContainer();
+    bool encode(AVFormatContext* ofmt_ctx, AVCodecContext* codec_ctx, AVFrame* frame, AVPacket* packet, AVStream* stream, bool rescale);
+    bool setupVideo();
+    bool setupAudio();
+    bool setupContainer();
 
-	AVFormatContext* fmt_ctx;
-	AVStream* video_stream;
-	AVCodec* vcodec;
-	AVCodecContext* vcodec_ctx;
-	AVFrame* video_frame;
-	AVFrame* sws_frame;
-	SwsContext* sws_ctx;
-	AVStream* audio_stream;
-	AVCodec* acodec;
-	AVFrame* audio_frame;
-	AVFrame* swr_frame;
-	AVCodecContext* acodec_ctx;
-	AVPacket video_pkt;
-	AVPacket audio_pkt;
-	SwrContext* swr_ctx;
+    AVFormatContext* fmt_ctx = nullptr;
+    AVStream* video_stream= nullptr;
+    AVCodec* vcodec= nullptr;
+    AVCodecContext* vcodec_ctx= nullptr;
+    AVFrame* video_frame= nullptr;
+    AVFrame* sws_frame= nullptr;
+    SwsContext* sws_ctx= nullptr;
+    AVStream* audio_stream= nullptr;
+    AVCodec* acodec= nullptr;
+    AVFrame* audio_frame= nullptr;
+    AVFrame* swr_frame= nullptr;
+    AVCodecContext* acodec_ctx= nullptr;
+    AVPacket video_pkt;
+    AVPacket audio_pkt;
+    SwrContext* swr_ctx = nullptr;
 
-	bool vpkt_alloc;
-	bool apkt_alloc;
+    bool vpkt_alloc;
+    bool apkt_alloc;
 
-	int aframe_bytes;
-	int ret;
-	char* c_filename;
+    int aframe_bytes;
+    int ret;
+    char* c_filename;
 
-	QMutex mutex;
-	QWaitCondition waitCond;
+    QMutex mutex;
+    QWaitCondition waitCond;
 };
 
 #endif // EXPORTTHREAD_H
