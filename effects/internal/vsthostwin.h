0ae1279735016282d38a151071938dcabca1f0f3
Jonathan Noble <jonnobleuk@gmail.com>
2019-01-27 12:40:50 +0000

    ifdef check on vsthostwin for win32

--- effects/internal/vsthostwin.h
+++ effects/internal/vsthostwin.h
@@ -1,5 +1,6 @@
 #ifndef VSTHOSTWIN_H
 #define VSTHOSTWIN_H
+#ifdef _WIN32
 
 #include "project/effect.h"
 
@@ -12,38 +13,40 @@ struct AEffect;
 class QDialog;
 
 class VSTHostWin : public Effect {
-	Q_OBJECT
+    Q_OBJECT
 public:
-	VSTHostWin(Clip* c, const EffectMeta* em);
-	~VSTHostWin();
-	void process_audio(double timecode_start, double timecode_end, quint8* samples, int nb_bytes, int channel_count);
+    VSTHostWin(ClipPtr c, const EffectMeta* em);
+    virtual ~VSTHostWin();
+    void process_audio(double timecode_start, double timecode_end, quint8* samples, int nb_bytes, int channel_count);
 
-	void custom_load(QXmlStreamReader& stream);
-	void save(QXmlStreamWriter& stream);
+    void custom_load(QXmlStreamReader& stream);
+    void save(QXmlStreamWriter& stream);
 private slots:
-	void show_interface(bool show);
-	void uncheck_show_button();
-	void change_plugin();
+    void show_interface(bool show);
+    void uncheck_show_button();
+    void change_plugin();
 private:
-	EffectField* file_field;
+    EffectField* file_field;
 
-	void loadPlugin();
-	void freePlugin();
-	dispatcherFuncPtr dispatcher;
-	AEffect* plugin;
-	bool configurePluginCallbacks();
-	void startPlugin();
-	void stopPlugin();
-	void resumePlugin();
-	void suspendPlugin();
-	bool canPluginDo(char *canDoString);
-	void initializeIO();
-	void processAudio(long numFrames);
-	float** inputs;
-	float** outputs;
-	QDialog* dialog;
-	QPushButton* show_interface_btn;
-	HMODULE modulePtr;
+    void loadPlugin();
+    void freePlugin();
+    dispatcherFuncPtr dispatcher;
+    AEffect* plugin;
+    bool configurePluginCallbacks();
+    void startPlugin();
+    void stopPlugin();
+    void resumePlugin();
+    void suspendPlugin();
+    bool canPluginDo(char *canDoString);
+    void initializeIO();
+    void processAudio(long numFrames);
+    float** inputs;
+    float** outputs;
+    QDialog* dialog;
+    QPushButton* show_interface_btn;
+    HMODULE modulePtr;
 };
+
+#endif
 
 #endif // VSTHOSTWIN_H
