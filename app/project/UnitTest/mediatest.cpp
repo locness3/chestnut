41c5e1e6ae177f78e07b268c846947db14c8c748
Jon Noble <jonnoble@gmail.com>
2019-02-07 19:25:29 +0000

    Media clear unit-test

--- app/project/UnitTest/mediatest.cpp
+++ app/project/UnitTest/mediatest.cpp
@@ -10,53 +10,63 @@ MediaTest::MediaTest()
 
 void MediaTest::testCaseConstructor()
 {
-    Media mda;
-    QVERIFY(mda.getId() == 1);
-    QVERIFY(mda.get_object<Footage>() == nullptr);
-    QVERIFY(mda.get_object<Sequence>() == nullptr);
-    QVERIFY(mda.get_type() == MediaType::NONE);
-    QVERIFY(mda.get_name().isEmpty());
-    QVERIFY(mda.childCount() == 0);
-    QVERIFY(mda.child(0) == nullptr);
-    QVERIFY(mda.columnCount() == 3);
-    QVERIFY(mda.data(0,0).isValid());
-    QVERIFY(mda.row() == 0);
-    QVERIFY(mda.parentItem() == nullptr);
+  Media mda;
+  QVERIFY(mda.getId() == 1);
+  QVERIFY(mda.object<Footage>() == nullptr);
+  QVERIFY(mda.object<Sequence>() == nullptr);
+  QVERIFY(mda.get_type() == MediaType::NONE);
+  QVERIFY(mda.get_name().isEmpty());
+  QVERIFY(mda.childCount() == 0);
+  QVERIFY(mda.child(0) == nullptr);
+  QVERIFY(mda.columnCount() == 3);
+  QVERIFY(mda.data(0,0).isValid());
+  QVERIFY(mda.row() == 0);
+  QVERIFY(mda.parentItem() == nullptr);
 }
 
 
 void MediaTest::testCaseConstructorWithParent()
 {
-    auto parent = std::make_shared<Media>();
-    Media child(parent);
-    QVERIFY(child.parentItem() != nullptr);
-    QVERIFY(child.parentItem() == parent);
+  auto parent = std::make_shared<Media>();
+  Media child(parent);
+  QVERIFY(child.parentItem() != nullptr);
+  QVERIFY(child.parentItem() == parent);
 }
 
 
 void MediaTest::testCaseSetAsFootage()
 {
-    Media mda;
-    auto ftg = std::make_shared<Footage>();
-    mda.set_footage(ftg);
-    QVERIFY(mda.get_type() == MediaType::FOOTAGE);
-    QVERIFY(mda.get_object<Footage>() != nullptr);
+  Media mda;
+  auto ftg = std::make_shared<Footage>();
+  mda.set_footage(ftg);
+  QVERIFY(mda.get_type() == MediaType::FOOTAGE);
+  QVERIFY(mda.get_object<Footage>() != nullptr);
 }
 
 void MediaTest::testCaseSetAsSequence()
 {
-    Media mda;
-    auto sqn = std::make_shared<Sequence>();
-    mda.set_sequence(sqn);
-    QVERIFY(mda.get_type() == MediaType::SEQUENCE);
-    QVERIFY(mda.get_object<Sequence>() != nullptr);
+  Media mda;
+  auto sqn = std::make_shared<Sequence>();
+  mda.set_sequence(sqn);
+  QVERIFY(mda.get_type() == MediaType::SEQUENCE);
+  QVERIFY(mda.get_object<Sequence>() != nullptr);
 }
 
 
 void MediaTest::testCaseSetAsFolder()
 {
-    Media mda;
-    mda.set_folder();
-    QVERIFY(mda.get_type() == MediaType::FOLDER);
-    QVERIFY(!mda.get_name().isEmpty());
+  Media mda;
+  mda.set_folder();
+  QVERIFY(mda.get_type() == MediaType::FOLDER);
+  QVERIFY(!mda.get_name().isEmpty());
+}
+
+void MediaTest::testCaseClear()
+{
+  Media mda;
+  auto ftg = std::make_shared<Footage>();
+  mda.set_footage(ftg);
+  mda.clear_object();
+  QVERIFY(mda.get_type() == MediaType::NONE);
+  QVERIFY(mda.get_object<Footage>() == nullptr);
 }
--- app/project/UnitTest/mediatest.h
+++ app/project/UnitTest/mediatest.h
@@ -12,12 +12,32 @@ public:
 
 signals:
 
-private slots:
+private slots:    
+    /**
+     * @brief Construct default media instance and check class attributes are correct
+     */
     void testCaseConstructor();
-    void testCaseConstructorWithParent();
+    /**
+     * @brief Set media instance as a child of another instance and check linkage is correct
+     */
+    void testCaseConstructorWithParent();    
+    /**
+     * @brief Set media instance as a Footage type and check stored object is non-null
+     */
     void testCaseSetAsFootage();
+    /**
+     * @brief Set media instance as a Sequence type and check stored object is non-null
+     */
     void testCaseSetAsSequence();
+    /**
+     * @brief Set media instance as a folder and check name is non-empty
+     */
     void testCaseSetAsFolder();
+
+    /**
+     * @brief Clearing of media sets it to none-type and the stored object is empty/null/invalid
+     */
+    void testCaseClear();
 };
 
 #endif // MEDIATEST_H
