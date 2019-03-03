9ef68322b674694748b9caaebd74332d4d2fc2ba
Jonathan Noble <jonnobleuk@gmail.com>
2019-03-03 13:20:16 +0000

    Using <random> instead of rand()

--- app/effects/internal/shakeeffect.cpp
+++ app/effects/internal/shakeeffect.cpp
@@ -1,7 +1,7 @@
 /* 
  * Olive. Olive is a free non-linear video editor for Windows, macOS, and Linux.
  * Copyright (C) 2018  {{ organization }}
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -31,63 +31,62 @@
 #include "debug.h"
 
 ShakeEffect::ShakeEffect(ClipPtr c, const EffectMeta *em) : Effect(c, em) {
-	enable_coords = true;
+  enable_coords = true;
 
-    EffectRowPtr intensity_row = add_row(tr("Intensity"));
-	intensity_val = intensity_row->add_field(EffectFieldType::DOUBLE, "intensity");
-	intensity_val->set_double_minimum_value(0);
+  EffectRowPtr intensity_row = add_row(tr("Intensity"));
+  intensity_val = intensity_row->add_field(EffectFieldType::DOUBLE, "intensity");
+  intensity_val->set_double_minimum_value(0);
 
-    EffectRowPtr rotation_row = add_row(tr("Rotation"));
-	rotation_val = rotation_row->add_field(EffectFieldType::DOUBLE, "rotation");
-	rotation_val->set_double_minimum_value(0);
+  EffectRowPtr rotation_row = add_row(tr("Rotation"));
+  rotation_val = rotation_row->add_field(EffectFieldType::DOUBLE, "rotation");
+  rotation_val->set_double_minimum_value(0);
 
-    EffectRowPtr frequency_row = add_row(tr("Frequency"));
-	frequency_val = frequency_row->add_field(EffectFieldType::DOUBLE, "frequency");
-	frequency_val->set_double_minimum_value(0);
+  EffectRowPtr frequency_row = add_row(tr("Frequency"));
+  frequency_val = frequency_row->add_field(EffectFieldType::DOUBLE, "frequency");
+  frequency_val->set_double_minimum_value(0);
 
-	// set defaults
-	intensity_val->set_double_default_value(25);
-	rotation_val->set_double_default_value(10);
-	frequency_val->set_double_default_value(5);
+  // set defaults
+  intensity_val->set_double_default_value(25);
+  rotation_val->set_double_default_value(10);
+  frequency_val->set_double_default_value(5);
 
-	srand(QDateTime::currentMSecsSinceEpoch());
-
-	for (int i=0;i<RANDOM_VAL_SIZE;i++) {
-		random_vals[i] = (double)rand()/RAND_MAX;
-	}
+  const auto limit = std::numeric_limits<int32_t>::max();
+  for (int i=0;i<RANDOM_VAL_SIZE;i++) {
+    random_vals[i] = static_cast<double>(this->randomNumber<int32_t>()) / limit;
+  }
 }
 
 void ShakeEffect::process_coords(double timecode, GLTextureCoords& coords, int) {
-	int lim = RANDOM_VAL_SIZE/6;
+  int lim = RANDOM_VAL_SIZE/6;
 
-	double multiplier = intensity_val->get_double_value(timecode)/lim;
-	double rotmult = rotation_val->get_double_value(timecode)/lim/10;
-	double x = timecode * frequency_val->get_double_value(timecode);
+  double multiplier = intensity_val->get_double_value(timecode)/lim;
+  double rotmult = rotation_val->get_double_value(timecode)/lim/10;
+  double x = timecode * frequency_val->get_double_value(timecode);
 
-	double xoff = 0;
-	double yoff = 0;
-	double rotoff = 0;
+  double xoff = 0;
+  double yoff = 0;
+  double rotoff = 0;
 
-	for (int i=0;i<lim;i++) {
-		int offset = 6*i;
-		xoff += qSin((x+random_vals[offset])*random_vals[offset+1]);
-		yoff += qSin((x+random_vals[offset+2])*random_vals[offset+3]);
-		rotoff += qSin((x+random_vals[offset+4])*random_vals[offset+5]);
-	}
+  for (int i=0;i<lim;i++) {
+    int offset = 6*i;
+    xoff += qSin((x+random_vals[offset])*random_vals[offset+1]);
+    yoff += qSin((x+random_vals[offset+2])*random_vals[offset+3]);
+    rotoff += qSin((x+random_vals[offset+4])*random_vals[offset+5]);
+  }
 
-	xoff *= multiplier;
-	yoff *= multiplier;
-	rotoff *= rotmult;
+  xoff *= multiplier;
+  yoff *= multiplier;
+  rotoff *= rotmult;
 
-	coords.vertexTopLeftX += xoff;
-	coords.vertexTopRightX += xoff;
-	coords.vertexBottomLeftX += xoff;
-	coords.vertexBottomRightX += xoff;
+  coords.vertexTopLeftX += xoff;
+  coords.vertexTopRightX += xoff;
+  coords.vertexBottomLeftX += xoff;
+  coords.vertexBottomRightX += xoff;
 
-	coords.vertexTopLeftY += yoff;
-	coords.vertexTopRightY += yoff;
-	coords.vertexBottomLeftY += yoff;
-	coords.vertexBottomRightY += yoff;
+  coords.vertexTopLeftY += yoff;
+  coords.vertexTopRightY += yoff;
+  coords.vertexBottomLeftY += yoff;
+  coords.vertexBottomRightY += yoff;
 
-	glRotatef(rotoff, 0, 0, 1);
+  glRotatef(rotoff, 0, 0, 1);
 }
