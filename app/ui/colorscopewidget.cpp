ec6920e2eecefe498afcce5d82afb8d29908f545
Jonathan Noble <jonnobleuk@gmail.com>
2019-04-04 18:45:08 +0100

    Optimisations to color scope

--- app/ui/colorscopewidget.cpp
+++ app/ui/colorscopewidget.cpp
@@ -7,6 +7,16 @@ using ui::ColorScopeWidget;
 
 constexpr int HORIZONTAL_STEP = 8;
 constexpr int PEN_ALPHA = 24;
+constexpr int MINOR_GRID_STEP = 8;
+constexpr int MAJOR_GRID_STEP = MINOR_GRID_STEP / 2;
+
+namespace {
+  const QPen r_pen(QColor(255,0,0,PEN_ALPHA));
+  const QPen g_pen(QColor(0,255,0,PEN_ALPHA));
+  const QPen b_pen(QColor(0,0,255,PEN_ALPHA));
+  const QPen bk_pen(Qt::black);
+  const QPen bka_pen(QColor(0,0,0, 128));
+}
 
 ColorScopeWidget::ColorScopeWidget(QWidget *parent) : QWidget(parent)
 {
@@ -26,54 +36,49 @@ void ColorScopeWidget::updateImage(QImage img)
 void ColorScopeWidget::paintEvent(QPaintEvent*/*event*/)
 {
   QPainter painter(this);
-  QPen r_pen(QColor(255,0,0,PEN_ALPHA));
-  QPen g_pen(QColor(0,255,0,PEN_ALPHA));
-  QPen b_pen(QColor(0,0,255,PEN_ALPHA));
-  painter.setRenderHint(QPainter::Antialiasing);
   // clear last paint
   painter.eraseRect(0, 0, width(), img_.height());
 
   const auto w_step = img_.width() / width();
+  int pos;
+  QRgb val;
 
-  // far too slow to be done for every pixel
+  // FIXME: far too slow to be done for every pixel
+  // with QImage::setPixel the blending of values is lost
   for (auto w = 0; w < width(); ++w) {
     for (auto h = 0; h < img_.height(); h+=HORIZONTAL_STEP) {
       // draw pixel value (per channel) on y-axis at x-position
-      auto val = img_.pixel(w * w_step, h);
-      auto r_pos = qRed(val);
-      auto g_pos = qGreen(val);
-      auto b_pos = qBlue(val);
+      val = img_.pixel(w * w_step, h);
+      pos = qRed(val);
       painter.setPen(r_pen);
-      painter.drawPoint(w, height() - r_pos);
+      painter.drawPoint(w, height() - pos);
+      pos = qGreen(val);
       painter.setPen(g_pen);
-      painter.drawPoint(w, height() - g_pos);
+      painter.drawPoint(w, height() - pos);
+      pos = qBlue(val);
       painter.setPen(b_pen);
-      painter.drawPoint(w, height() - b_pos);
+      painter.drawPoint(w, height() - pos);
     }
   }
 
   // paint surrounding box
-  QPen pen(Qt::black);
-  painter.setPen(pen);
+  painter.setPen(bk_pen);
   painter.drawRect(0, 0, width(), height()-1);
 
-  //FIXME: do this in 1 loop and don't paint minor over major
-  // major lines
-  pen.setColor(QColor(0,0,0, 128));
-  painter.setPen(pen);
-  int h_step = height() / 4;
-  for (auto h = h_step; h < height(); h+=h_step) {
-    painter.drawLine(0, height() - h, width(), height() -  h);
-  }
-
-  // minor lines
+  // paint grid-lines
   QVector<qreal> dashes;
   dashes << 3 << 3;
-  pen.setDashPattern(dashes);
-  painter.setPen(pen);
-  h_step = height() / 8;
-  for (auto h = h_step; h < height(); h+=h_step) {
+  QPen minor_pen(bka_pen);
+  minor_pen.setDashPattern(dashes);
+  const int major_step = height() / MAJOR_GRID_STEP;
+  const int minor_step = height() / MINOR_GRID_STEP;
+
+  for (auto h = minor_step; h < height(); h+=minor_step) {
+    if (h % major_step == 0) {
+      painter.setPen(bka_pen);
+    } else {
+      painter.setPen(minor_pen);
+    }
     painter.drawLine(0, height() - h, width(), height() -  h);
   }
-
 }
