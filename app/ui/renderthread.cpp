57fd1b3965da944975833a1ba21494be09aa29a9
Jonathan Noble <jonnobleuk@gmail.com>
2019-04-05 13:15:40 +0100

    Texture height was being assigned the sequence width

--- app/ui/renderthread.cpp
+++ app/ui/renderthread.cpp
@@ -65,7 +65,7 @@ void RenderThread::run() {
             glBindTexture(GL_TEXTURE_2D, texColorBuffer);
             glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, sequenceNow->width(), sequenceNow->height(), 0, GL_RGB, GL_UNSIGNED_BYTE, nullptr);
             tex_width = sequenceNow->width();
-            tex_height = sequenceNow->width();
+            tex_height = sequenceNow->height();
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             ctx->functions()->glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texColorBuffer, 0);
