7c3f4fe04ece772baf63e67a99eadd3019cf5234
Jonathan Noble <jonnobleuk@gmail.com>
2019-04-09 11:44:15 +0100

    Missed new files

---
+++ app/effects/internal/diptocolourtransition.cpp
@@ -0,0 +1,13 @@
+#include "diptocolourtransition.h"
+
+namespace  {
+  const QColor DIP_COLOUR(255,255,255,255);
+}
+
+using fx::DipToColourTransition;
+
+DipToColourTransition::DipToColourTransition(ClipPtr c, ClipPtr s, const EffectMeta* em) : Transition(c, s, em)
+{
+
+}
+
---
+++ app/effects/internal/diptocolourtransition.h
@@ -0,0 +1,14 @@
+#ifndef DIPTOCOLOURTRANSITION_H
+#define DIPTOCOLOURTRANSITION_H
+
+#include "project/transition.h"
+
+namespace fx
+{
+  class DipToColourTransition : public Transition
+  {
+    public:
+      DipToColourTransition(ClipPtr c, ClipPtr s, const EffectMeta* em);
+  };
+}
+#endif // DIPTOCOLOURTRANSITION_H
