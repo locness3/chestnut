ebeb6e12b8676ecdd8c38df780ba685c01545469
Jonathan Noble <jonnobleuk@gmail.com>
2019-05-10 09:25:05 +0100

    About dialog changes

--- app/dialogs/aboutdialog.cpp
+++ app/dialogs/aboutdialog.cpp
@@ -1,7 +1,7 @@
 /* 
  * Olive. Olive is a free non-linear video editor for Windows, macOS, and Linux.
  * Copyright (C) 2018  {{ organization }}
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -22,33 +22,32 @@
 #include <QDialogButtonBox>
 
 AboutDialog::AboutDialog(QWidget *parent) :
-	QDialog(parent)
+  QDialog(parent)
 {
-	setWindowTitle("About Olive");
-	setMaximumWidth(360);
+  setWindowTitle("About Chestnut");
+  setMaximumWidth(360);
 
-	QVBoxLayout* layout = new QVBoxLayout();
-	layout->setSpacing(20);
-	setLayout(layout);
+  QVBoxLayout* layout = new QVBoxLayout();
+  layout->setSpacing(20);
+  setLayout(layout);
 
-    QLabel* label =
-            new QLabel("<html><head/><body>"
-                       "<p><img src=\":/icons/olive-splash.png\"/></p>"
-                       "<p><a href=\"https://www.olivevideoeditor.org/\">"
-                       "<span style=\" text-decoration: underline; color:#007af4;\">"
-                       "https://www.olivevideoeditor.org/"
-                       "</span></a></p><p>"
-                       + tr("Olive is a non-linear video editor. This software is free and protected by the GNU GPL.")
-                       + "</p><p>"
-                       + tr("Olive Team is obliged to inform users that Olive source code is available for download from its website.")
-                       + "</p></body></html>");
-	label->setAlignment(Qt::AlignCenter);
-	label->setWordWrap(true);
-	layout->addWidget(label);
+  QLabel* label =
+      new QLabel("<html><head/><body>"
+                 "<p><a href=\"https://github.com/jonno85uk/chestnut\">"
+                 "<span style=\" text-decoration: underline; color:#007af4;\">"
+                 "https://github.com/jonno85uk/chestnut"
+                 "</span></a></p><p>"
+                 + tr("Chestnut is a non-linear video editor. This software is free and protected by the GNU GPL.")
+                 + "</p><p>"
+                 + tr("Chestnut source code is available for download from its website.")
+                 + "</p></body></html>");
+  label->setAlignment(Qt::AlignCenter);
+  label->setWordWrap(true);
+  layout->addWidget(label);
 
-	QDialogButtonBox* buttons = new QDialogButtonBox(QDialogButtonBox::Ok, this);
-	buttons->setCenterButtons(true);
-	layout->addWidget(buttons);
+  QDialogButtonBox* buttons = new QDialogButtonBox(QDialogButtonBox::Ok, this);
+  buttons->setCenterButtons(true);
+  layout->addWidget(buttons);
 
-	connect(buttons, SIGNAL(accepted()), this, SLOT(accept()));
+  connect(buttons, SIGNAL(accepted()), this, SLOT(accept()));
 }
